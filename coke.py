27322bff1b8cb5c5d05e38daf9a8bffbd070858a
Koushal S Modi <124229657+koushalsmodi@users.noreply.github.com>
2024-07-25 15:49:22 -0700

    Update coke.pye

--- coke.py
+++ coke.py
@@ -19,3 +19,22 @@ def amount_due():
 
 
 main()
+
+
+def main():
+     print('Amount Due: ', 50)
+     amount_due()
+
+def amount_due():
+     change = 50
+
+     while change !=0:
+          insert = int(input('Insert Coin: '))
+          if insert in [25, 10, 5]:
+               change -= insert
+               print('Amount Due: ', change)
+          else:
+               print('Amount Due: ', change)
+               print('Error invalid coin entered')
+     print('Change Owed: ', change)
+     
